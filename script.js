3f8edb2cdd89772eaf6043662fd2dba625c95bc2
fslipe17 <128160248+fslipe17@users.noreply.github.com>
2023-10-06 12:03:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function pedir(){
+    var opção = prompt("Escolha uma opção de 1 á 5")
+    switch(Number(opção)){
+        case 1:
+            alert("Você escolheu Refri de Guaraná")
+            break;
+        case 2:
+            alert("Você escolheu Refri de Laranja")
+            break;
+        case 3:
+            alert("Você escolheu Refri de Uva")
+            break;
+        case 4:
+            alert("Você escolheu Refri de Limão")
+            break;
+        case 5:
+            alert("Você escolheu Coca-Cola")
+            break;
+    }
+}
